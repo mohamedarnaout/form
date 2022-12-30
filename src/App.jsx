a86b2d73af2bad911d5ecea531b4532f84972328
“mohamed <[mohamedearna@gmail.com]>
2022-12-30 08:44:03 -0600

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function App() {
+  // State to store the form data and any errors
+
+  // Render the form
+  return (
+    <form action="">
+      <div className="firstname">
+        <labal></labal>
+        <input type="text" />
+      </div>
+      <div className="email">
+        <label htmlFor="">email</label>
+        <input type="email" name="" id="" />
+      </div>
+      <div className="password">
+        <label htmlFor="">Password :</label>
+        <input type="password" name="" id="" />
+      </div>
+    </form>
+  );
+}
+export default App;
